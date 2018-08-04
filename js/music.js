7a2103d20649e890756b316c5db079447c89add3
evenyao <40289169+evenyao@users.noreply.github.com>
2018-08-04 15:00:48 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -9,7 +9,7 @@ function $$(selectorAll){
 function getMusicList(callback){
   //ajax获取数据
   var xhr = new XMLHttpRequest()
-  xhr.open('GET','/music.json',true)
+  xhr.open('GET','/music-player/music.json',true)
   xhr.onload = function(){
     if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
       // console.log(JSON.parse(this.responseText))
